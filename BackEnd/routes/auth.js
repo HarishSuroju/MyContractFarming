ed9348bef57fbccd48af0e97acb12d0a4e6721c0
HarishSuroju <harishsurojuv@gmail.com>
2026-01-08 21:23:32 +0530

    Second commit

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser, getUserProfile } = require('../controllers/authController');
+const { registerUser, loginUser, getUserProfile, verifyOTP, resendOTP } = require('../controllers/authController');
 const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 // Public routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/verify-otp', verifyOTP);
+router.post('/resend-otp', resendOTP);
 
 // Protected routes
 router.get('/profile', authenticateToken, getUserProfile);
